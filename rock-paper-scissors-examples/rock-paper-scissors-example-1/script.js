0028ad6dd991eb9d5361544893d9fff859f9ed89
rosepetaldrift <rosesophiewright@gmail.com>
2022-04-09 00:43:21 -0500

    Rock Paper Scissors

--- rock-paper-scissors-examples/rock-paper-scissors-example-1/script.js
+++ rock-paper-scissors-examples/rock-paper-scissors-example-1/script.js
@@ -2,6 +2,13 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+const rock = document.getElementById("computerRock");
+const scissors = document.getElementById("computerScissors");
+const paper = document.getElementById("computerPaper");
+const userScoreDisplay = document.getElementById("userScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let userScore = 0;
+let computerScore = 0;
 let userChoice
 let computerChoice
 let result
@@ -18,12 +25,24 @@ function generateComputerChoice() {
   
   if (randomNumber === 1) {
     computerChoice = 'rock'
+    paper.style.display = 'none'
+    scissors.style.display = 'none'
+    rock.style.display = 'block'
+    console.log(rock);
   }
   if (randomNumber === 2) {
     computerChoice = 'scissors'
+    paper.style.display = 'none'
+    scissors.style.display = 'block'
+    rock.style.display = 'none'
+    console.log(scissors);
   }
   if (randomNumber === 3) {
     computerChoice = 'paper'
+    paper.style.display = 'block'
+    scissors.style.display = 'none'
+    rock.style.display = 'none'
+    console.log(paper);
   }
   computerChoiceDisplay.innerHTML = computerChoice
 }
@@ -33,22 +52,33 @@ function getResult() {
     result = 'its a draw!'
   }
   if (computerChoice === 'rock' && userChoice === "paper") {
-    result = 'you win!'
+    result = 'you win!';
+    userScore++;
+    console.log(userScore)
+
   }
   if (computerChoice === 'rock' && userChoice === "scissors") {
-    result = 'you lost!'
+    result = 'you lose!';
+    computerScore++;
   }
   if (computerChoice === 'paper' && userChoice === "scissors") {
-    result = 'you win!'
+    result = 'you win!';
+    userScore++;
   }
   if (computerChoice === 'paper' && userChoice === "rock") {
-    result = 'you lose!'
+    result = 'you lose!';
+    computerScore++;
   }
   if (computerChoice === 'scissors' && userChoice === "rock") {
     result = 'you win!'
+    userScore++;
   }
   if (computerChoice === 'scissors' && userChoice === "paper") {
-    result = 'you lose!'
+    result = 'you lose!';
+    computerScore++;
   }
-  resultDisplay.innerHTML = result
+  resultDisplay.innerHTML = result;
+  userScoreDisplay.innerHTML = userScore;
+  computerScoreDisplay.innerHTML = computerScore;
+
 }
